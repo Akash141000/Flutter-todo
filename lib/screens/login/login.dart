58495ef922bb2a9a422e26e0fcbe725172a9be37
Akash Padwal <akashpadwal21@gmail.com>
2022-11-13 13:38:19 +0530

    login and signup page created

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/models/auth.model.dart';
+
+const loginRoute = '/login';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("LOGIN");
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        height: double.infinity,
+        width: double.infinity,
+        child: BlocBuilder<AuthBloc, Auth>(
+          builder: ((context, authState) =>
+              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 5),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Email'),
+                      contentPadding: EdgeInsets.symmetric(vertical: 25),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 5),
+                  child: TextFormField(
+                    obscureText: true,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    decoration: const InputDecoration(
+                        label: Text('Password'),
+                        contentPadding: EdgeInsets.symmetric(vertical: 25)),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      debugPrint('LOGIN');
+                      context.read<AuthBloc>().setToken('LOGIN');
+                      Navigator.pushReplacementNamed(context, '/');
+                    },
+                    child: const Text('SUBMIT'),
+                  ),
+                ),
+              ])),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/signup/signup.dart
+++ lib/screens/signup/signup.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
+const registerRoute = '/register';
+
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("SIGN-UP");
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Email'),
+                contentPadding: EdgeInsets.symmetric(vertical: 25),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            child: TextFormField(
+              obscureText: true,
+              autocorrect: false,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                  label: Text('Password'),
+                  contentPadding: EdgeInsets.symmetric(vertical: 25)),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            child: TextFormField(
+              obscureText: true,
+              autocorrect: false,
+              enableSuggestions: false,
+              decoration: const InputDecoration(
+                  label: Text('Confirm Password'),
+                  contentPadding: EdgeInsets.symmetric(vertical: 25)),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: ElevatedButton(
+              onPressed: () => debugPrint('LOGIN'),
+              child: const Text('SUBMIT'),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
